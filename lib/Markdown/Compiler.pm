1694187d311348c0f813b5216fe66933aa290f2c
Kaitlyn Parkhurst <symkat@symkat.com>
2021-03-12 09:55:06 -0800

    Building documentation.

--- lib/Markdown/Compiler.pm
+++ lib/Markdown/Compiler.pm
@@ -67,3 +67,62 @@ has metadata => (
 );
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Markdown::Compiler - A Markdown Compiler
+
+=head1 DESCRIPTION
+
+Markdown::Compiler is a malleable markdown parser and compiler.
+
+=head1 SYNOPSIS
+
+=head1 CONSTRUCTOR
+
+=head2 source
+
+=head2 lexer
+
+=head2 parser
+
+=head2 compiler
+
+=head1 METHODS
+
+=head2 lexer
+
+=head2 parser
+
+=head2 stream
+
+=head2 tree
+
+=head2 compiler
+
+=head2 result
+
+=head2 metadata
+
+=head1 AUTHOR
+
+Kaitlyn Parkhurst (SymKat) I<E<lt>symkat@symkat.comE<gt>> ( Blog: L<http://symkat.com/> )
+
+=head1 CONTRIBUTORS
+
+=head1 COPYRIGHT
+
+Copyright (c) 2021 the Markdown::Compiler L</AUTHOR> and L</CONTRIBUTORS> as listed above.
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms as perl itself.
+
+=head1 AVAILABILITY
+
+The most current version of Markdown::Compiler can be found at L<https://github.com/symkat/Markdown-Compiler>
+
